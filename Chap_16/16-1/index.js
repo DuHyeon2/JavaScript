606a8aec18022e243433ff35bd2309a6ca859ba8
Kim Du Hyeon <83499405+DuHyeon2@users.noreply.github.com>
2022-11-11 17:09:20 +0900

    update

---
+++ Chap_16/16-1/index.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+    var req = new XMLHttpRequest()
+    req.onreadystatechange = function(){
+        if(req.readyState==4){
+            if(req.status == 200){
+                document.getElementById('view').innerHTML = req.responseText
+            }
+        }
+    }
+    req.open('GET', 'data.txt')
+    req.send(null)
+}
